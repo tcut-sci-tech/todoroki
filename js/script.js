b3b0a883116fef893b5d4d110891fb7a9366912f
tcut-sci-tech <88429838+tcut-sci-tech@users.noreply.github.com>
2022-04-16 12:59:32 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,41 +1,13 @@
-$(function(){
-	
-	var
-	  winW = $(window).width(),
-		winH = $(window).height(),
-		nav = $('#mainnav ul a'),
-		curPos = $(this).scrollTop();
-	
-	if (winW > 880){
-		var headerH =20;
-	}
-	else{
-		var headerH =60;
-	}
-	
-	$(nav).on('click', function(){
-		nav.removeClass('active');
-  	var $el = $(this),
-		id = $el.attr('href');
- 		$('html, body').animate({
-   		scrollTop: $(id).offset().top - headerH
- 		}, 500);
-		$(this).addClass('active');
-		if (winW < 880){
-			$('#menuWrap').next().slideToggle();
-			$('#menuBtn').removeClass('close');
-		}
- 		return false;
-	});
-	
-	$('.panel').hide();
-	$('#menuWrap').toggle(function(){
-		$(this).next().slideToggle();
-		$('#menuBtn').toggleClass('close');
-	},
-	function(){
-		$(this).next().slideToggle();
-		$('#menuBtn').removeClass('close');
-	});
-
+/*---------- スライド ---------- */
+$('.slider').slick({
+  autoplay: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  prevArrow: '<div class="slick-prev"></div>',
+  nextArrow: '<div class="slick-next"></div>',
+  centerMode: true,
+  variableWidth: true,
+  dots: false,
 });
