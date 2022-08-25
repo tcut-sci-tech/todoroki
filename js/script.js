8f522f609dd69fc25ab696cc3f8eba7de7081556
tcut-sci-tech <88429838+tcut-sci-tech@users.noreply.github.com>
2022-08-25 17:23:03 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+/*---------- ロード ---------- */
+$(window).on('load',function(){
+  $("#splash").delay(1500).fadeOut('slow');
+  $("#splash_logo").delay(1200).fadeOut('slow');
+});
+
 /*---------- スライド ---------- */
 $('.slider').slick({
   autoplay: true,
